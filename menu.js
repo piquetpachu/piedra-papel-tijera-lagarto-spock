e341cb1dc00d595b1c2323a0202f44125f765a8a
piquetpachu <pachu510121@gmail.com>
2024-03-04 17:01:59 -0300

    segundo commit

---
+++ menu.js
@@ -0,0 +1,10 @@
+function mostrarimg(){
+    var container = document.getElementById("imagenregla")[0];
+
+    if (container.style.visibility == "hidden"){
+        container.style.visibility = "visible";
+    }else{
+        container.style.visibility = "hidden";
+    }
+}
+
